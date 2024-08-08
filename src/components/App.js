ba239d5d15e6b224f1d2740671d05d3ef0e89c8a
Samson Njai <njaisamson@gmail.com>
2024-08-08 15:56:17 +0300

    Modifies the App.js

--- src/components/App.js
+++ src/components/App.js
@@ -25,7 +25,6 @@ function App() {
   return (
     <div className={`App ${isDarkMode ? "dark" : "light"}`}>
       <Header onDarkModeClick={onDarkModeClick} isDarkMode={isDarkMode} />
-      <Filter onCategoryChange={handleCategoryChange} />
       <ShoppingList items={itemsToDisplay} />
     </div>
   );
